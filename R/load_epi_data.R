4f8bcd99bb2dd2f933c447d8394b93da8a6fb95b
Hugo Gruson <hugo.gruson@protonmail.com>
2020-11-30 16:51:05 +0100

    Only keep selected countries in load_epi_data()

--- R/load_epi_data.R
+++ R/load_epi_data.R
@@ -1,10 +1,26 @@
-load_epi_data <- function() {
+load_epi_data <- function(end_date = Sys.Date()) {
 
   read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/data.csv") %>%
     filter(continentExp == "Europe") %>%
     transmute(Date = as.Date(paste(year, month, day, sep = "-")),
               NewCases = cases,
               NewDeaths = deaths,
-              Country = countriesAndTerritories)
+              Country = countriesAndTerritories) %>%
+    filter(Country %in% c("Austria",
+                          "Belgium", "Bulgaria",
+                          "Croatia", "Cyprus", "Czechia",
+                          "Denmark",
+                          "Estonia",
+                          "Finland", "France",
+                          "Germany", "Greece",
+                          "Hungary",
+                          "Iceland", "Ireland", "Italy",
+                          "Latvia", "Lithuania", "Luxembourg",
+                          "Malta",
+                          "Netherlands", "Norway",
+                          "Poland", "Portugal",
+                          "Romania",
+                          "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland",
+                          "United Kingdom"))
 
 }
