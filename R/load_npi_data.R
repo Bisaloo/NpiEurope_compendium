3df43e1b3fe82178428cfb6342bf3210c2956743
Hugo Gruson <hugo.gruson@protonmail.com>
2020-11-30 15:42:00 +0100

    Create first functional prototype of load_npi_data()

---
+++ R/load_npi_data.R
@@ -0,0 +1,26 @@
+load_npi_data <- function(end_date = Sys.Date()) {
+
+  end_date <- as.Date(end_date)
+
+  npi_data <- read.csv("https://www.ecdc.europa.eu/sites/default/files/documents/response_graphs_2020-11-26.csv") %>%
+    mutate(across(c(date_start, date_end), as.Date)) %>%
+    mutate(date_end = if_else(is.na(date_end), end_date, date_end))
+
+  start_date <- min(npi_data$date_start, npi_data$date_start, na.rm = TRUE)
+
+  skeleton <- merge(
+    data.frame("Date" = seq(start_date, end_date, by = 1)),
+    data.frame("Country" = unique(npi_data$Country))
+  )
+
+  npi_data <- merge(npi_data, skeleton) %>%
+    group_by(Country) %>%
+    mutate(in_use = Date >= date_start & Date <= date_end) %>%
+    group_by(Country, Date, Response_measure) %>%
+    summarise(in_use = any(in_use)) %>%
+    pivot_wider(names_from = Response_measure, values_from = in_use) %>%
+    mutate(across(!any_of(c("Country", "Date")), isTRUE)) %>%
+    mutate(across(!any_of(c("Country", "Date")), as.numeric))
+
+  return(npi_data)
+}
