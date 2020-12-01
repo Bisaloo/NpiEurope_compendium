1df145d48ba01d27f38eb8131841596f0000ea0d
Hugo Gruson <hugo.gruson@protonmail.com>
2020-12-01 13:44:58 +0100

    Memoise create_summary_matrix()

--- R/zzz.R
+++ R/zzz.R
@@ -4,5 +4,8 @@
   load_npi_data <<- memoise::memoise(load_npi_data)
   load_epi_data <<- memoise::memoise(load_epi_data)
 
+  # Memoise slow function
+  create_summary_matrix <<- memoise::memoise(create_summary_matrix)
+
 }
 
