6ed670f75c79300ab334cf0553c84e500db9d6ac
Hugo Gruson <hugo.gruson@protonmail.com>
2020-12-02 14:23:19 +0100

    Register variables used in non-standard evaluation

--- R/zzz.R
+++ R/zzz.R
@@ -7,5 +7,11 @@
   # Memoise slow function
   create_summary_matrix <<- memoise::memoise(create_summary_matrix)
 
+  # Fix warnings due to non-standard evaluation
+  utils::globalVariables(
+    c("Country", "cur_column", "strats", "NbStrategies",
+      "X2.5.", "X97.5.", "start", "end")
+  )
+
 }
 
