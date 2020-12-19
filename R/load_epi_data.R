08a0d2ceb4e6ffad8e66b6873983b8cdb53bfe70
Hugo Gruson <hugo.gruson@protonmail.com>
2020-12-19 15:51:47 +0100

    Add end_date arg to load_epi_data() to be able to analyse to first wave only

--- R/load_epi_data.R
+++ R/load_epi_data.R
@@ -1,5 +1,7 @@
 #' Get epidemiological data for 31 European countries
 #'
+#' @inheritParams load_npi_data
+#'
 #' @references
 #' https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
 #'
@@ -7,9 +9,10 @@
 #' @importFrom dplyr %>% filter transmute arrange
 #'
 #' @export
-load_epi_data <- function() {
+load_epi_data <- function(end_date = Sys.Date()){
 
   read.csv(system.file("extdata", "epi_europe.csv", package = "NpiEurope"),
-           colClasses = c("Date", "integer", "integer", "character"))
+           colClasses = c("Date", "integer", "integer", "character")) %>%
+    filter(Date <= end_date)
 
 }
