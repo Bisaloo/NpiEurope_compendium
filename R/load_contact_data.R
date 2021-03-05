29f14060617c49c1e42d92f30014a50bab0a3744
Hugo Gruson <hugo.gruson@protonmail.com>
2021-03-05 17:58:16 +0100

    Transform contactdata in the dedicated function

--- R/load_contact_data.R
+++ R/load_contact_data.R
@@ -14,18 +14,21 @@
 load_contact_data <- function(country) {
 
   if (country == "Norway") {
-    return(contact_matrix("Sweden"))
+    contact_data <- contact_matrix("Sweden")
+  } else if (country == "Liechtenstein") {
+    contact_data <- contact_matrix("Luxembourg")
+  } else if (country == "United Kingdom") {
+    contact_data <- contact_matrix("UK")
+  } else {
+    contact_data <- contact_matrix(country)
   }
 
-  if (country == "Liechtenstein") {
-    return(contact_matrix("Luxembourg"))
+  contact_data2 <- matrix(NA_real_, ncol = 8, nrow = 8)
+  for (i in 1:8) {
+    for (j in 1:8) {
+      contact_data2[i, j] <- sum(contact_data[c((i - 1) * 2 + 1, (i - 1) * 2 + 2), c((j - 1) * 2 + 1, (j - 1) * 2 + 2)])
+    }
   }
 
-  if (country == "United Kingdom") {
-    return(contact_matrix("UK"))
-  }
-
-  return(contact_matrix(country))
-
-
+  return(contact_data2)
 }
